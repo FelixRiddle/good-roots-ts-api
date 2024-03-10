2cd03a4175812dd51864fe8ad20ef1f767246d7e
Felix <fedecraft3312@gmail.com>
2024-03-10 15:07:36 -0300

    Now it seems to work, however I used 'npm link', don't know if it works without it

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
 import ExpressAuthentication from "./api/auth/ExpressAuthentication";
 import FrontendAuthAPI from "./api/auth/FrontendAuthAPI";
 import SERVER_URL_MAPPINGS from "./mappings/env/SERVER_URL_MAPPINGS";
-// import AuthMarkupController from "./lib/frontendMarkupController/AuthMarkupController";
 
-import UserData from "./types/UserData";
-
-export {
+export default {
     // Api's
-    // AuthMarkupController,
     ExpressAuthentication,
     FrontendAuthAPI,
     
     // Config/Maps
     SERVER_URL_MAPPINGS,
-    
-    // Types
-    UserData,
 }
